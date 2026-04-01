1813ac6ef153f83de032a88788c3aa953ac25e7f
airqx <s202270440@kfupm.edu.sa>
2026-04-01 08:59:13 +0300

    update parser

--- utils/parser.js
+++ utils/parser.js
@@ -1,8 +1,13 @@
 import _ from "lodash";
 
 export function parseNumbers(input) {
-  const numbers = _.map(input, (str) => Number(str));
-return numbers.filter((num) => !Number.isNaN(num));}
+  const numbers = _.map(input, (str) => {
+    const num = Number(str);
+    return Number.isNaN(num) ? null : num;
+  });
+
+  return _.compact(numbers);
+}
 
 export function isValidOperation(operation) {
   const validOps = ["add", "subtract", "multiply", "divide"];
